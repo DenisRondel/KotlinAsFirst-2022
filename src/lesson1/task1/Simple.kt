d4d802ecf9949f46a079e62917df1b6bc214fe47
denis <denis.rondel@rambler.ru>
2022-09-09 17:17:26 +0300

    zadachki

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -82,7 +82,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (deg + sec/3600 + min/60)* PI/ 180 // дорешать
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double =  (deg + sec.toDouble()/3600 + min.toDouble()/60)* PI/ 180 // дорешать
 
 /**
  * Тривиальная (1 балл)
@@ -90,7 +90,7 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (deg + sec/3600 + min/
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double =
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(y2-y1)+ sqr(x2-x1))
 
 /**
  * Простая (2 балла)
@@ -98,7 +98,11 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double =
  * Пользователь задает целое число, больше или равно 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Any =
+    if (number>99) {
+        val number = number%1000
+        number/100
+    } else print("а так делать нельзя :)")
 
 /**
  * Простая (2 балла)
@@ -107,7 +111,7 @@ fun thirdDigit(number: Int): Int = TODO()
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =(hoursArrive*60 + minutesArrive)-(hoursDepart*60+minutesDepart)
 
 /**
  * Простая (2 балла)
@@ -117,6 +121,8 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
 fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+}
+
 
 /**
  * Простая (2 балла)
