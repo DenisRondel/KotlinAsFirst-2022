9531561db54b13299a1d176302a7eaa4f7477fca
denis <denis.rondel@rambler.ru>
2022-12-23 02:12:00 +0300

    lesson 7

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -302,7 +302,7 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
 fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
     var a = 0
     var b = 1
-    if (list.isEmpty() || (list.max() == 0 && number == 0)) return -1 to -1
+    if (list.isEmpty()) return -1 to -1
     while ((list[a] + list[b] != number && a != b) || a != list.size - 1) {
         b += 1
         if (b == list.size) {
